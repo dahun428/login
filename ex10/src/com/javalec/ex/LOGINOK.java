0c25f21d1e5cdcf54d4a630487357080937c1b58
dahun428 <rubcustomer@gmail.com>
2020-01-08 11:49:03 +0900

    20200108 login 1

--- ex10/src/com/javalec/ex/LOGINOK.java
+++ ex10/src/com/javalec/ex/LOGINOK.java
@@ -5,8 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
- 
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
